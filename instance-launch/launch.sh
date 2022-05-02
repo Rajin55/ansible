e716837e9d0a21d511a197467b16542c3278705a
saisindhu96 <saisindhuraju@gmail.com>
2022-05-02 10:58:39 +0530

    Added new line comment

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -15,7 +15,7 @@ DNS_UPDATE(){
  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
  aws route53 change-resource-record-sets --hosted-zone-id Z0853472YEV9ELZNQML9 --change-batch file:///tmp/record.json | jq
 }
-INSTANCE_STATE=$(aws --regio us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].state.Name | xargs -n1 )
+INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].state.Name | xargs -n1 )
 if [ "${INSTANCE_STATE}" = "running" ]; then
   echo"Instance is already Existed"
   DNS_UPDATE
@@ -26,6 +26,6 @@ if [ "${INSTANCE_STATE}" = "stop" ]; then
   echo"Instance is already Existed"
 fi
 
-aws ec2 --regio us-east-1 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+aws ec2 --region us-east-1 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 sleep 30
 DNS_UPDATE
